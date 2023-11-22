8c3abd938f724cf205ae5570fd724566de4f1d6c
GustavoBonif <gubonic@gmail.com>
2023-11-22 17:38:55 -0300

    setup:creating Catalog microservice, that will contain brand and products

---
+++ src/main/java/com/diningroom/catalog/DiningRoomCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.diningroom.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningRoomCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiningRoomCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diningroom/catalog/DiningRoomCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diningroom.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningRoomCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
